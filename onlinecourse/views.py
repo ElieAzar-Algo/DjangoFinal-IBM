ebb23010e58ad359e5282e0f0d65dc41884efa0c
elieazar-algo <elie.azar3891.ea@gmail.com>
2023-06-02 06:52:18 -0400

    new changes

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
 import logging
+from .models import Course, Enrollment, Question, Choice, Submission
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 # Create your views here.
@@ -110,18 +111,23 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
-
+def submit(request, course_id):
+     course = get_object_or_404(Course, pk=course_id)
+     user = request.user
+     enrollment = Enrollment.objects.get(user=user, course=course)
+     submission = Submission.objects.create(enrollment=enrollment, choices=submitted_anwsers)
+     return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(submission.id,)))
+     
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+   submitted_anwsers = []
+   for key in request.POST:
+       if key.startswith('choice'):
+           value = request.POST[key]
+           choice_id = int(value)
+           submitted_anwsers.append(choice_id)
+   return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -131,6 +137,5 @@ def enroll(request, course_id):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 #def show_exam_result(request, course_id, submission_id):
-
 
 
