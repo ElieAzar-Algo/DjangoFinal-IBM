ff632ddb5a4cf51a68c50c94002b3d8369d1e26f
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2023-06-11 03:35:58 +0300

    lab 1 & 2 completed

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -5,17 +5,27 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice
 # <HINT> Register QuestionInline and ChoiceInline classes here
 class QuestionInline(admin.StackedInline):
     model = Question
-    extra = 5
+    extra = 3
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 5
+    extra = 3
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('content','course', 'grade' )
+    list_filter = ['content']
+    search_fields = ['content', 'course']
+    
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['content']
+    
+    
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-
+    
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
@@ -32,7 +42,7 @@ class LessonAdmin(admin.ModelAdmin):
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
